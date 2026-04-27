dc35879f96cdaa63dbc3c912dfbd61cdc1f75fc4
dimedical-corporativo <redessociales@dimedicalcorporativo.mx>
2026-04-27 16:09:38 -0600

    k8s structure

--- url-shortener/api-service/src/main/java/com/projects/api_service/ApiServiceApplication.java
+++ url-shortener/api-service/src/main/java/com/projects/api_service/ApiServiceApplication.java
@@ -3,6 +3,8 @@ package com.projects.api_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class ApiServiceApplication {
 
@@ -10,4 +12,9 @@ public class ApiServiceApplication {
 		SpringApplication.run(ApiServiceApplication.class, args);
 	}
 
+	@PostConstruct
+	public void debug() {
+		System.out.println("DB_HOST=" + System.getenv("DB_HOST"));
+		System.out.println("SPRING_URL=" + System.getProperty("spring.datasource.url"));
+	}	
 }
